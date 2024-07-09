b3f0fbd6db16eeeac13524351881ee7733d26431
ApA24091987 <168029434+ApA24091987@users.noreply.github.com>
2024-07-09 13:03:44 +0300

    Add files via upload

---
+++ form'str'.py
@@ -0,0 +1,21 @@
+team1_num = 6
+print('В команде Мавстера кода участников: %d !' % team1_num)
+
+team2_num = 6
+print('Итоги сегодня в командах участников: %d И %d !' % (team1_num, team2_num))
+
+score_2 = 42
+print('Команда Волшебники данных решил задача: {} !' .format(score_2))
+
+team1_time = 1552.512
+print('Волшебники данных решили задачи за {:.2f} C !'.format(team1_time))
+
+score_1 = 40
+print(f'Команда решила {score_1} И {score_2} задач.')
+
+challenge_result = 'Победа команды Волшебники данных!'
+print(f'Результат битвы: {challenge_result}')
+
+tasks_total = 82
+time_avg = 45.2
+print(f'Сегодня было решено {tasks_total} задач, в среднем по {time_avg:.1f} секунды на задачу!.')
